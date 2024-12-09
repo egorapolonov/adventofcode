245ad97472ad5e8bef6259b5ccc22ee050f51c45
egorapolonov <yehor.apolonov@innovecs.com>
2024-12-08 16:29:52 -0800

    https://adventofcode.com/2024/day/7
    - mostly drafts, need to refactor as well

--- src/main/java/com/adventofcode/day7/Day7_2.java
+++ src/main/java/com/adventofcode/day7/Day7_2.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 import com.adventofcode.utils.FileUtils;
 
-public class Day7_1 {
+public class Day7_2 {
 
     public static void main(String[] args) throws Exception {
-        new Day7_1().count();
+        new Day7_2().count();
     }
 
     public void count() throws Exception {
@@ -19,18 +19,20 @@ public class Day7_1 {
        /* long expected = 292;
         List<Long> values = List.of(11, 6, 16, 20);
         Node node = new Node().calculate(expected, values);
-        System.out.println("calibrated = " + node.calibrated);*/
+        //System.out.println("calibrated = " + node.calibrated);*/
 
         List<Calibration> calibrations = loadCalibrations();
         long count = 0;
         for (Calibration calibration : calibrations) {
+            //if (calibration.expected != 192) continue;;
             Node node = new Node().calculate(calibration.expected, calibration.values);
             if (node.calibrated) {
                 count += node.expected;
             }
         }
         System.out.println("final answer = " + count);
-        // answer is 4998764814652
+        // answer for tmp 11387
+        // answer 37599040735984 is too high
     }
 
     protected static class Node {
@@ -41,6 +43,7 @@ public class Day7_1 {
         long val;
         Node mul;
         Node sum;
+        Node concat;
         boolean calibrated;
 
         @Override
@@ -53,22 +56,29 @@ public class Day7_1 {
 
         }
 
-        Node calculate(long expected, List<Long> values) {
+        protected Node calculate(long expected, List<Long> values) {
             Node root = new Node();
             root.index = 0;
             root.expected = expected;
-            root.val = values.get(index);
-            root.exp = String.valueOf(val);
+            root.val = values.get(root.index);
+            root.exp = String.valueOf(root.val);
             root.sum = sum(root, values);
             root.mul = mul(root, values);
-            if (root.sum != null && root.sum.calibrated || root.mul != null && root.mul.calibrated) {
-                root.calibrated = true;
+            root.concat = concat(root, values);
+            if(root.sum != null && root.sum.calibrated) {
+                root.calibrated=true;
+            }
+            if(root.mul != null && root.mul.calibrated) {
+                root.calibrated=true;
+            }
+            if(root.concat != null && root.concat.calibrated) {
+                root.calibrated=true;
             }
             return root;
         }
 
         Node sum(Node prev, List<Long> values) {
-            //System.out.println("prev " + prev);
+            //System.out.println("SUM prev " + prev);
             if (prev.index >= values.size() - 1) {
                 return null;
             }
@@ -80,17 +90,25 @@ public class Day7_1 {
             retVal.val = prev.val + val;
             retVal.sum = sum(retVal, values);
             retVal.mul = mul(retVal, values);
-            if (retVal.isAnswer()) {
-                retVal.calibrated = true;
-                System.out.println("answer = " + retVal.exp + ", calibrated = " + prev);
+            retVal.concat = concat(retVal, values);
+            if (retVal.isAnswer() && isLastIndex(values, retVal)) {
+                retVal.calibrated=true;
+                //System.out.println("answer SUM = " + retVal.exp + ", calibrated = " + retVal);
             }
-            if (retVal.sum != null && retVal.sum.calibrated || retVal.mul != null && retVal.mul.calibrated) {
-                prev.calibrated = true;
+            if(retVal.sum != null && retVal.sum.calibrated) {
+                prev.calibrated=true;
+            }
+            if(retVal.mul != null && retVal.mul.calibrated) {
+                prev.calibrated=true;
+            }
+            if(retVal.concat != null && retVal.concat.calibrated) {
+                prev.calibrated=true;
             }
             return retVal;
         }
 
         Node mul(Node prev, List<Long> values) {
+            //System.out.println("MUL prev " + prev);
             if (prev.index >= values.size() - 1) {
                 return null;
             }
@@ -102,14 +120,58 @@ public class Day7_1 {
             retVal.val = prev.val * val;
             retVal.sum = sum(retVal, values);
             retVal.mul = mul(retVal, values);
-            if (retVal.isAnswer()) {
-                prev.calibrated = true;
-                System.out.println("answer = " + retVal.exp + ", calibrated = " + prev);
+            retVal.concat = concat(retVal, values);
+            if (retVal.isAnswer() && isLastIndex(values, retVal)) {
+                retVal.calibrated=true;
+                //System.out.println("answer SUM = " + retVal.exp + ", calibrated = " + retVal);
             }
-            if (retVal.sum != null && retVal.sum.calibrated || retVal.mul != null && retVal.mul.calibrated) {
-                prev.calibrated = true;
+            if(retVal.sum != null && retVal.sum.calibrated) {
+                prev.calibrated=true;
+            }
+            if(retVal.mul != null && retVal.mul.calibrated) {
+                prev.calibrated=true;
+            }
+            if(retVal.concat != null && retVal.concat.calibrated) {
+                prev.calibrated=true;
             }
             return retVal;
+        }
+
+        Node concat(Node prev, List<Long> values) {
+            //System.out.println("MUL prev " + prev);
+            if (prev.index >= values.size() - 1) {
+                return null;
+            }
+            Node retVal = new Node();
+            retVal.index = prev.index + 1;
+            retVal.expected = prev.expected;
+            long prevVal = prev.val;
+            long currVal = values.get(retVal.index);
+            long val = Long.parseLong("%d%d".formatted(prevVal, currVal));
+            //System.out.println("concat = %s, %s, %s".formatted(prevVal, currVal, val));
+            retVal.exp = prev.exp + "||" + currVal;
+            retVal.val = val;
+            retVal.sum = sum(retVal, values);
+            retVal.mul = mul(retVal, values);
+            retVal.concat = concat(retVal, values);
+            if (retVal.isAnswer() && isLastIndex(values, retVal)) {
+                retVal.calibrated=true;
+                //System.out.println("answer CONCAT = " + retVal.exp + ", calibrated = " + retVal);
+            }
+            if(retVal.sum != null && retVal.sum.calibrated) {
+                prev.calibrated=true;
+            }
+            if(retVal.mul != null && retVal.mul.calibrated) {
+                prev.calibrated=true;
+            }
+            if(retVal.concat != null && retVal.concat.calibrated) {
+                prev.calibrated=true;
+            }
+            return retVal;
+        }
+
+        private boolean isLastIndex(List<Long> values, Node retVal) {
+            return retVal.index == values.size() - 1;
         }
 
         boolean isAnswer() {
@@ -136,7 +198,7 @@ public class Day7_1 {
     protected List<Calibration> loadCalibrations() throws Exception {
         try (BufferedReader br = new BufferedReader(
                 new InputStreamReader(FileUtils.resourceFileToInputStream("day7_1.txt")))) {
-                //new InputStreamReader(FileUtils.resourceFileToInputStream("day7_1_tmp.txt")))) {
+            //new InputStreamReader(FileUtils.resourceFileToInputStream("day7_1_tmp.txt")))) {
             List<Calibration> retVal = new ArrayList<>();
             String calibrationLine = null;
             while ((calibrationLine = br.readLine()) != null && calibrationLine.length() != 0) {
